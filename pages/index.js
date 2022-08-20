6e4380e75ab106cf2e4fc1e9bba761c2e64c7d91
kyokusoradio <kyokuso.radio@gmail.com>
2022-08-20 17:26:07 +0900

    commit01

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import React from 'react'
+
+export default function Home() { 
+  return (
+    <>
+      <header>HEADER</header>
+      <main>
+        <div>
+          <h1>CUBE</h1>
+          <p>アウトプットしていくサイト</p>
+        </div>
+      </main>
+      <footer>FOOTER</footer>
+    </>
+  )
+}
