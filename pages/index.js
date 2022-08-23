e3bb790d2cfdeda9e34e4eb195dade4aeac9bbbf
旭操ラジオ <kyokuso.radio0612@gmail.com>
2022-08-23 14:13:45 +0000

    make blogs directry about page

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,10 @@ import styles from "../styles/Home.module.css";
 import React from "react";
 
 import Hero from "components/hero";
-import Layout from "components/layout";
 
 export default function Home() {
-  return (
-    <>
-      <Layout>
-        <Hero />
-      </Layout>
-    </>
-  );
+    return <Hero 
+      title="CUBE"
+     subtitle="アウトプットしていくサイト"
+    />
 }
