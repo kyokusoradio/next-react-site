a05c48d9bf1123bcd5b9264d27c57b5d434e82d4
旭操ラジオ <kyokuso.radio0612@gmail.com>
2022-08-24 14:29:40 +0000

    update 5files

--- components/hero.js
+++ components/hero.js
@@ -5,9 +5,9 @@ export default function Hero({title, subtitle, imageOn = false}) {
     <div className={styles.flexContainer}>
       <div className={styles.text}>
         <h1 className={styles.title}>{title}</h1>
-        <p className={styles.subtitle}>{subtitle}</p>
-        {imageOn && <figure>[画像]</figure>}
+        <p className={styles.subtitle}>{subtitle}</p>  
       </div>
+      {imageOn && <figure>[画像]</figure>}
     </div>
   )
 }
