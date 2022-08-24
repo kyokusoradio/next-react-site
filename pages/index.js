a3eec139763276c057b001400c6a59a234e0de82
旭操ラジオ <kyokuso.radio0612@gmail.com>
2022-08-24 00:48:32 +0000

    Update hero.js and index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,8 @@ import Hero from "components/hero";
 
 export default function Home() {
     return <Hero 
-      title="CUBE"
-     subtitle="アウトプットしていくサイト"
+        title="CUBE"
+        subtitle="アウトプットしていくサイト"
+        imageOn
     />
 }
