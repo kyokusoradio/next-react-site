a4adda639e48d8ee62e9754c565fc3f140d54948
旭操ラジオ <kyokuso.radio0612@gmail.com>
2022-08-24 00:56:42 +0000

    Update 4 files

---
+++ components/nav.js
@@ -0,0 +1,29 @@
+import Link from "next/link"
+
+export default function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link href="/">
+                        <a>Home</a>
+                    </Link>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <Link href="/about">
+                        <a>About</a>
+                    </Link>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <Link href="/blog">
+                        <a>Blog</a>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
