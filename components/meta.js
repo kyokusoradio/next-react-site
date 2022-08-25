b159d5187c440815e7d62ede6e136a132b65c039
旭操ラジオ <kyokuso.radio0612@gmail.com>
2022-08-25 13:20:20 +0000

    update meta dates

--- components/meta.js
+++ components/meta.js
@@ -4,11 +4,17 @@ import {useRouter} from "next/router"
 import {siteMeta} from "lib/constants"
 const {siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon} = siteMeta
 
-export default function Meta({pageTitle, pageDesc}) {
+import siteImg from 'images/ogp.jpg'
+
+export default function Meta({pageTitle, pageDesc, pageImg, pageImgW, pageImgH }) {
     const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
     const desc = pageDesc ?? siteDesc
     const router = useRouter()
     const url = `${siteUrl}${router.asPath}`
+    const img = pageImg || siteImg.src
+    const imgW = pageImgW || siteImg.width
+    const imgH = pageImgH || siteImg.height
+    const imgUrl = img.startsWith('https') ? img : `${siteUrl}${img}`
     
     return (
         <Head>
@@ -23,7 +29,10 @@ export default function Meta({pageTitle, pageDesc}) {
             <meta property="og:local" content={siteLocale} />
             <link rel="icon" href={siteIcon} />
             <link rel="apple-touch-icon" href={siteIcon} />
-                
+            <meta property="og:image" content={imgUrl} />
+            <meta property="og:image:width" content={imgW} />
+            <meta property="og:image:height" content={imgH} />
+            <meta name="twitter:card" content="summary_large_image" />
         </Head>
     )
 }
