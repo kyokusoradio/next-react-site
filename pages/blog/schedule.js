99502f1a6f39fcbd1a1af82a6043a68aec764942
旭操ラジオ <kyokuso.radio0612@gmail.com>
2022-08-25 14:01:36 +0000

    update some files

---
+++ pages/blog/schedule.js
@@ -0,0 +1,16 @@
+import { client } from "lib/api"
+
+export default function Schedule() {
+  return<h1>記事のタイトル</h1>
+}
+
+export async function getStaticProps() {
+  const resPromise = client.get({
+    endpoint: 'blogs',
+  })
+  resPromise.then((res) => console.log(res)).catch((err) => console.log(err))
+
+  return {
+    props: {},
+  }
+}
