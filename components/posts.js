5540285601331a97be620669b9590285ee058f8a
旭操ラジオ <kyokuso.radio0612@gmail.com>
2022-08-27 13:38:41 +0000

    fix blog/index page

--- components/posts.js
+++ components/posts.js
@@ -4,24 +4,12 @@ import Image from 'next/image'
 
 export default function Posts({ posts }) {
     return (
-        <div className={styles.gridContainer}>
-            {posts.map(({title, slug, eyecatch}) => (
+        <div className={styles.gridConatiner}>
+            {posts.map(({ title, slug }) => (
                 <article className={styles.post} key={slug}>
                     <Link href={`/blog/${slug}`}>
                         <a>
-                            <figure>
-                                <Image 
-                                    src={eyecatch.url}
-                                    alt=""
-                                    layout='responsive'
-                                    width={eyecatch.width}
-                                    height={eyecatch.height}
-                                    sizes="(min-width: 1152px) 576px, 50vw"
-                                    plaiceholder="blur"
-                                    blurDataUrl={eyecatch.blurDataURL}
-                                 />
-                            </figure>
-                            <h2>{`${title}${slug}`}</h2>
+                            <h2>{title}</h2>
                         </a>
                     </Link>
                 </article>
