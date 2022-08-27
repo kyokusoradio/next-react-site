2b7c7d1ff49047e92615d69e21632b6a86a39983
旭操ラジオ <kyokuso.radio0612@gmail.com>
2022-08-27 13:54:22 +0000

    setup homepage posts

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
+import { getAllPosts } from 'lib/api';
 import Meta from 'components/meta'
 import Container from 'components/container'
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import React from "react";
+import { getPlaiceholder } from 'plaiceholder';
+import { eyecatchLocal } from 'lib/constants';
 
 import Hero from "components/hero";
+import Posts from 'components/posts';
+import Pagination from 'components/pagination';
 
-export default function Home() {
+export default function Home({ posts }) {
     return (
         <Container>
             <Meta />
@@ -16,6 +21,26 @@ export default function Home() {
                 subtitle="アウトプットしていくサイト"
                 imageOn
             />
+            <Posts posts={posts} />
+            <Pagination nextUrl='/blog' nextText='More Posts' />
         </Container>
     )
+}
+
+export async function getStaticProps() {
+    const posts = await getAllPosts(4)
+
+    for (const post of posts) {
+        if (!post.hasOwnProperty('eyecatch')) {
+            post.eyecatch = eyecatchLocal
+        }
+        const { base64 } = await getPlaiceholder(post.eyecatch.url)
+        post.eyecatch.blurDataURL = base64
+    }
+
+    return {
+        props: {
+            posts: posts,
+        }
+    }
 }
