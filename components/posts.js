4ded966942563820a05ccc241bd249bdee469b1b
旭操ラジオ <kyokuso.radio0612@gmail.com>
2022-08-27 13:38:58 +0000

    fix blog page

--- components/posts.js
+++ components/posts.js
@@ -4,11 +4,22 @@ import Image from 'next/image'
 
 export default function Posts({ posts }) {
     return (
-        <div className={styles.gridConatiner}>
-            {posts.map(({ title, slug }) => (
+        <div className={styles.gridContainer}>
+            {posts.map(({ title, slug, eyecatch }) => (
                 <article className={styles.post} key={slug}>
                     <Link href={`/blog/${slug}`}>
                         <a>
+                            <figure>
+                                <Image 
+                                    src={eyecatch.url}
+                                    alt=''
+                                    layout="fill"
+                                    ofject="cover"
+                                    sizes="(min-width: 1152px) 576px, 50vw"
+                                    placeholder="blur"
+                                    blurDataURL={eyecatch.blurDataURL}
+                                 />
+                            </figure>
                             <h2>{title}</h2>
                         </a>
                     </Link>
