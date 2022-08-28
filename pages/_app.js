bf2fcb722f4aa014bc15f24653d5ae92cc1a9c02
旭操ラジオ <kyokuso.radio0612@gmail.com>
2022-08-28 14:55:31 +0000

    setup gtag 404

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,48 @@
+import { useEffect } from 'react'
+import { useRouter } from 'react'
 import '../styles/globals.css'
 import Layout from 'components/layout' 
+import Script from 'next/script'
+import * as gtag from 'lib/gtag'
 
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
-    <Layout>
+    <>
+      <Script 
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MESUREMENT_ID}`}
+      />
+      <Script 
+      id='gtag-init'
+      strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_MESUREMENT_ID}');
+          `,
+        }}
+      />
+      <Layout>
       <Component {...pageProps} />
     </Layout>
+    </>
+    
   )
 }
 
